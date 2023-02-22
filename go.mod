584740d92b6e6ab1d611972591176ef75b3dae73
PotatoCloud <xiaofeng@tzai.net>
2023-02-22 09:43:34 +0800

    update handshake

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module CVLAN
 
 go 1.20
 
-require golang.org/x/crypto v0.6.0 // indirect
+require golang.org/x/crypto v0.6.0
--- handshake.go
+++ handshake.go
@@ -15,7 +15,7 @@ type ServerHandshake struct{}
 type serverHandshakeWithECDH struct{ *Conn }
 
 func (e *serverHandshakeWithECDH) Do() error {
-	// recv alice pub key
+	// recv alice public key
 	apk, err := e.ReadAsBytes()
 	if err != nil {
 		return err
@@ -65,12 +65,12 @@ func (e *serverHandshakeWithVerify) Do() error {
 		return err
 	}
 
-	// check server hello
+	// check client hello
 	if util.Bytes2String(data) != "Hello,Server" {
-		return errors.New("error server hello")
+		return errors.New("error client hello")
 	}
 
-	// create client hello
+	// create server hello
 	if data, err = e.crypt.Encrypt(util.String2Bytes("Hello,Client"), nil); err != nil {
 		return err
 	}
@@ -79,5 +79,80 @@ func (e *serverHandshakeWithVerify) Do() error {
 	return err
 }
 
-func (ServerHandshake) Verify(conn *Conn) HandShake { return &serverHandshakeWithVerify{conn} }
 func (ServerHandshake) ECDH(conn *Conn) HandShake   { return &serverHandshakeWithECDH{conn} }
+func (ServerHandshake) Verify(conn *Conn) HandShake { return &serverHandshakeWithVerify{conn} }
+
+type ClientHandshake struct{}
+
+type clientHandshakeWithECDH struct{ *Conn }
+
+func (e *clientHandshakeWithECDH) Do() (err error) {
+	ecdh, _ := crypto.NewCurve25519ECDH()
+
+	// send alice public key
+	if _, err = e.WriteAsBytes(ecdh.Marshal()); err != nil {
+		return
+	}
+
+	// recv bob public key
+	bpk, err := e.ReadAsBytes()
+	if err != nil {
+		return
+	}
+
+	if len(bpk) != 32 {
+		return errors.New("invalid read size")
+	}
+
+	// parse bob public key
+	bobPub, err := ecdh.Unmarshal(bpk)
+	if err != nil {
+		return err
+	}
+
+	sharedKey, err := ecdh.GenerateShared(bobPub)
+	if err != nil {
+		return err
+	}
+	e.SessionSecret = *sharedKey
+
+	// setup crypt
+	if e.crypt, err = crypto.NewGCM(*sharedKey); err != nil {
+		return
+	}
+
+	return nil
+}
+
+type clientHandshakeWithVerify struct{ *Conn }
+
+func (e *clientHandshakeWithVerify) Do() error {
+	// create client hello
+	data, err := e.crypt.Encrypt(util.String2Bytes("Hello,Server"), nil)
+	if err != nil {
+		return err
+	}
+
+	if _, err = e.WriteAsBytes(data); err != nil {
+		return err
+	}
+
+	bs, err := e.ReadAsBytes()
+	if err != nil {
+		return err
+	}
+
+	if data, err = e.crypt.Decrypt(bs, nil); err != nil {
+		return err
+	}
+
+	// check server hello
+	if util.Bytes2String(data) != "Hello,Client" {
+		return errors.New("error server hello")
+	}
+
+	return nil
+}
+
+func (ClientHandshake) ECDH(conn *Conn) HandShake   { return &clientHandshakeWithECDH{conn} }
+func (ClientHandshake) Verify(conn *Conn) HandShake { return &clientHandshakeWithVerify{conn} }
